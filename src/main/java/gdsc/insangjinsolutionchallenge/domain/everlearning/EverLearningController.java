7dfede6a2da29fa269ef77a16a8f599acc8a60ee
sangjin <62700860+sangjin6439@users.noreply.github.com>
2024-04-10 17:53:21 +0900

    Update EverLearningController.java

--- src/main/java/gdsc/insangjinsolutionchallenge/domain/everlearning/EverLearningController.java
+++ src/main/java/gdsc/insangjinsolutionchallenge/domain/everlearning/EverLearningController.java
@@ -1,33 +1,27 @@
 package gdsc.insangjinsolutionchallenge.domain.everlearning;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-import java.time.Duration;
-
 @RestController
+@Slf4j
 public class EverLearningController {
 
-    private final WebClient webClient;
-
-    @Autowired
-    public EverLearningController(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl("https://apis.data.go.kr/7010000/everlearning").build();
-    }
+    private final WebClient webClient = WebClient.create();
 
     @GetMapping("/everlearning/web/{pageNum}")
-    public Mono<String> getLectureListWeb(@PathVariable("pageNum") int pageNum) {
-        //인증키를 URL에서 사용이 가능하도록 인코딩 된 인증키 값
-        String serviceKey = "ZRL2X90NTGx7ghsp6xtRE2IN5vsE%2FCgQTXtn8ZKUfvPMknH%2F0xf%2FZ3FfOxo2K%2BOeF%2Fa7DN2Fk7XwrCaV%2BSdegA%3D%3D";
+    public Mono<String> getLectureListWeb(@PathVariable("pageNum") int pageNum){
+        //인증키를 URL에서 사용이 가능하도록 인코딩 된 인증키 값을 넣어야함
+        String serviceKey = "VbXK1XrLoX1KFDCaxda0f%2FHxQxg1vLh8p0dQVeTrLAar27RnZl6tkQIwd2QL3n8b0tXCuRmzRXmCW%2FOS6aDK9Q%3D%3D";
         String pageNo = String.valueOf(pageNum);
         String numOfRows = "10";
-        String url = String.format("/getLectureList?ServiceKey=%s&pageNo=%s&numOfRows=%s",
+        String url = String.format("https://apis.data.go.kr/7010000/everlearning/getLectureList?ServiceKey=%s&pageNo=%s&numOfRows=%s",
                 serviceKey, pageNo, numOfRows);
-
+        log.info("url: {}", url);
         return webClient.get()
                 .uri(url) //API 엔드 포인트, 요청 URL설정
                 .retrieve() //요청 보내기 메서드
@@ -35,18 +29,17 @@ public class EverLearningController {
     }
 
     @GetMapping("/everlearning/app/{pageNum}")
-    public Mono<String> getLectureListApp(@PathVariable("pageNum") int pageNum) {
-        //인증키를 URL에서 사용이 가능하도록 인코딩 된 인증키 값
-        String serviceKey = "ZRL2X90NTGx7ghsp6xtRE2IN5vsE%2FCgQTXtn8ZKUfvPMknH%2F0xf%2FZ3FfOxo2K%2BOeF%2Fa7DN2Fk7XwrCaV%2BSdegA%3D%3D ";
+    public Mono<String> getLectureListApp(@PathVariable("pageNum") int pageNum){
+        //인증키를 URL에서 사용이 가능하도록 인코딩 된 인증키 값을 넣어야함
+        String serviceKey = "VbXK1XrLoX1KFDCaxda0f%2FHxQxg1vLh8p0dQVeTrLAar27RnZl6tkQIwd2QL3n8b0tXCuRmzRXmCW%2FOS6aDK9Q%3D%3D";
         String pageNo = String.valueOf(pageNum);
-        String numOfRows = "12";
-        String url = String.format("/getLectureList?ServiceKey=%s&pageNo=%s&numOfRows=%s",
+        String numOfRows = "15";
+        String url = String.format("https://apis.data.go.kr/7010000/everlearning/getLectureList?ServiceKey=%s&pageNo=%s&numOfRows=%s",
                 serviceKey, pageNo, numOfRows);
-
+        log.info("url: {}", url);
         return webClient.get()
                 .uri(url) //API 엔드 포인트, 요청 URL설정
                 .retrieve() //요청 보내기 메서드
-                .bodyToMono(String.class)// 응답 데이터를 Mono<T>로 변환
-                .timeout(Duration.ofSeconds(5));  //5초 이내로 응답이 안오면 timeout 발생 시킴
+                .bodyToMono(String.class); // 응답 데이터를 Mono<T>로 변환
     }
 }
